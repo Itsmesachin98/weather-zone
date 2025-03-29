d7e17495c53789a1589bb86537696a27055754d7
Sachin Kumar <sachinkumarskpog@gmail.com>
2025-03-29 19:43:53 +0530

    refactor: restructure weather app frontend and backend

--- backend/app.js
+++ backend/app.js
@@ -20,21 +20,62 @@ app.post("/", async (req, res) => {
 // This function sends the city name to openWeather and gives us the response
 const sendDataToOpenWeather = async (value) => {
     const apiKey = process.env.API_KEY;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${apiKey}&units=metric`;
+    let cityCode;
 
+    // Url to get city code
+    const cityCodeUrl = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${value}`;
     try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (err) {
-        console.log("Could not fetch data from OpenWeather");
+        const response = await fetch(cityCodeUrl);
+        console.log(response);
+        if (response.status === 200) {
+            const data = await response.json();
+
+            if (data === null) {
+                // The search bar is empty
+            } else if (data.length === 0) {
+                // The entered city is incorrect
+            } else {
+                cityCode = data[0].Key;
+                console.log(`${value}'s city code is ${cityCode}`);
+            }
+        } else {
+            console.log("Unable to fetch url");
+            return;
+        }
+    } catch (error) {
+        console.log("Error occurred while fetching city code ", error);
+        return;
     }
+
+    // Url to get weather info
+    try {
+        const weatherInfoUrl = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${cityCode}?apikey=${apiKey}&details=true&metric=true`;
+        const weatherInfo = await fetch(weatherInfoUrl);
+        const weatherData = await weatherInfo.json();
+        console.log(weatherData);
+    } catch (error) {
+        console.log("Error occurred while fetching weather info ", error);
+    }
+
+    // try {
+    //     let data = 400;
+    //     const response = await fetch(url);
+    //     if (response.status === 200) {
+    //         data = await response.json();
+    //     }
+    //     return data;
+    // } catch (err) {
+    //     console.log("Could not fetch data from OpenWeather");
+    // }
 };
 
 // app.listen(port, () => console.log(`Server started at port ${port}`));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    // console.log(`Server running on http://localhost:${PORT}`);
+    // console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
+
+// https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${apiKey}&units=metric
+// http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${value}&aqi=no
--- frontend/script.js
+++ frontend/script.js
@@ -1,257 +1,281 @@
-const card = document.getElementById("card");
+// const card = document.getElementById("card");
 const searchBtn = document.getElementById("search-btn");
+const cityName = document.getElementById("city-name");
+let allWeatherData;
 
 // This function will run whenever the page loads
 window.addEventListener("load", async () => {
-    createOverlayAndLoader();
-    createWeatherInfoCard();
-    const allWeatherData = await sendDataToBacked("Godda");
-    if (allWeatherData === 404) {
-        errorTemplate("images/error.png", "404 Error Occured!");
-    } else {
-        if (numberOfChildren() === 2) {
-            deleteWeatherInfo();
-        }
+    // createOverlayAndLoader();
+    // createWeatherInfoCard();
+    allWeatherData = await sendDataToBacked("Godda");
+    console.log(allWeatherData);
+    // if (allWeatherData === 404) {
+    //     errorTemplate("images/error.png", "404 Error Occured!");
+    // } else {
+    //     if (numberOfChildren() === 2) {
+    //         deleteWeatherInfo();
+    //     }
 
-        createCityAndTemp();
-        getDateAndTime();
-        updateCityAndTemp(allWeatherData);
-    }
-    deleteOverlayAndLoader();
+    //     // createCityAndTemp();
+    //     // getDateAndTime();
+    //     // updateCityAndTemp(allWeatherData);
+    // }
+    // deleteOverlayAndLoader();
 });
+
+// ============== EVENT LISTENERS ==============
+
+searchBtn.addEventListener("click", async () => {
+    // console.log(cityName.value);
+    allWeatherData = await sendDataToBacked(cityName.value);
+    console.log(allWeatherData);
+    updateWeatherInfo(allWeatherData);
+});
+
+// ============== FUNCTIONS ==============
+
+function updateWeatherInfo(weatherInfo) {
+    document.querySelector(".temp").innerHTML = `${Math.round(
+        weatherInfo.current.temp_c
+    )}&deg;C`;
+
+    document.querySelector(
+        ".weather-img"
+    ).src = `https:${weatherInfo.current.condition.icon}`;
+}
 
 /*
     This function will create something like this
     <div class="city-temp"></div>
 */
-const createWeatherInfoCard = () => {
-    const cityTemp = document.createElement("div");
-    cityTemp.classList.add("city-temp");
-    card.appendChild(cityTemp);
-};
+// const createWeatherInfoCard = () => {
+//     const cityTemp = document.createElement("div");
+//     cityTemp.classList.add("city-temp");
+//     card.appendChild(cityTemp);
+// };
 
-// This function triggers when the input field is clicked
-const cityName = document.getElementById("city-name");
-cityName.addEventListener("click", () => {
-    cityName.classList.add("click-animation");
+// // This function triggers when the input field is clicked
+// const cityName = document.getElementById("city-name");
+// cityName.addEventListener("click", () => {
+//     cityName.classList.add("click-animation");
 
-    document.body.addEventListener("click", (evt) => {
-        if (!cityName.contains(evt.target)) {
-            cityName.classList.remove("click-animation");
-        }
-    });
-});
+//     document.body.addEventListener("click", (evt) => {
+//         if (!cityName.contains(evt.target)) {
+//             cityName.classList.remove("click-animation");
+//         }
+//     });
+// });
 
-const months = {
-    0: "January",
-    1: "February",
-    2: "March",
-    3: "April",
-    4: "May",
-    5: "June",
-    6: "July",
-    7: "August",
-    8: "September",
-    9: "October",
-    10: "November",
-    11: "December",
-};
+// const months = {
+//     0: "January",
+//     1: "February",
+//     2: "March",
+//     3: "April",
+//     4: "May",
+//     5: "June",
+//     6: "July",
+//     7: "August",
+//     8: "September",
+//     9: "October",
+//     10: "November",
+//     11: "December",
+// };
 
-// This function updates the current date and time
-const getDateAndTime = () => {
-    const currDate = new Date();
-    const date = currDate.getDate();
-    const month = currDate.getMonth();
-    let hour = currDate.getHours();
-    const ampm = hour >= 12 ? "pm" : "am";
-    hour = hour % 12 || 12;
-    const minute = currDate.getMinutes();
+// // This function updates the current date and time
+// const getDateAndTime = () => {
+//     const currDate = new Date();
+//     const date = currDate.getDate();
+//     const month = currDate.getMonth();
+//     let hour = currDate.getHours();
+//     const ampm = hour >= 12 ? "pm" : "am";
+//     hour = hour % 12 || 12;
+//     const minute = currDate.getMinutes();
 
-    const dateTime = document.querySelector(".date-time");
-    dateTime.innerText = `${months[month].slice(
-        0,
-        3
-    )} ${date}, ${hour}:${minute}${ampm}`;
+//     const dateTime = document.querySelector(".date-time");
+//     dateTime.innerText = `${months[month].slice(
+//         0,
+//         3
+//     )} ${date}, ${hour}:${minute}${ampm}`;
 
-    return dateTime;
-};
+//     return dateTime;
+// };
 
-/*
-    This function will return the number of child element
-    stored inside the weather info card
-*/
-const numberOfChildren = () => {
-    const cityTemp = document.querySelector(".city-temp");
-    const numberOfChild = cityTemp.children.length;
-    return numberOfChild;
-};
+// /*
+//     This function will return the number of child element
+//     stored inside the weather info card
+// */
+// const numberOfChildren = () => {
+//     const cityTemp = document.querySelector(".city-temp");
+//     const numberOfChild = cityTemp.children.length;
+//     return numberOfChild;
+// };
 
-// This triggers when the search button is clicked
-searchBtn.addEventListener("click", async () => {
-    if (cityName.value.trim() !== "") {
-        createOverlayAndLoader();
-        const allWeatherData = await sendDataToBacked(cityName.value);
-        deleteOverlayAndLoader();
-        if (allWeatherData === 404) {
-            deleteWeatherInfo();
-            errorTemplate("images/error.png", "404 Error Occured!");
-        } else if (allWeatherData.cod === 200) {
-            if (numberOfChildren() === 2) {
-                deleteCityNotFoundTemplate();
-                createCityAndTemp();
-                getDateAndTime();
-                updateCityAndTemp(allWeatherData);
-            } else {
-                getDateAndTime();
-                updateCityAndTemp(allWeatherData);
-            }
-        } else {
-            deleteWeatherInfo();
-            errorTemplate("images/404-error.png", "City Not Found!");
-        }
-    }
-});
+// // This triggers when the search button is clicked
+// searchBtn.addEventListener("click", async () => {
+//     if (cityName.value.trim() !== "") {
+//         createOverlayAndLoader();
+//         const allWeatherData = await sendDataToBacked(cityName.value);
+//         deleteOverlayAndLoader();
+//         if (allWeatherData === 404) {
+//             deleteWeatherInfo();
+//             errorTemplate("images/error.png", "404 Error Occured!");
+//         } else if (allWeatherData.cod === 200) {
+//             if (numberOfChildren() === 2) {
+//                 deleteCityNotFoundTemplate();
+//                 createCityAndTemp();
+//                 getDateAndTime();
+//                 updateCityAndTemp(allWeatherData);
+//             } else {
+//                 getDateAndTime();
+//                 updateCityAndTemp(allWeatherData);
+//             }
+//         } else {
+//             deleteWeatherInfo();
+//             errorTemplate("images/404-error.png", "City Not Found!");
+//         }
+//     }
+// });
 
-/*
-    This function will create the child elements which will be stored
-    inside the weather info card.
+// /*
+//     This function will create the child elements which will be stored
+//     inside the weather info card.
 
-    This function will create something like this:
+//     This function will create something like this:
 
-    <span class="date-time">Date, Time</span>
-    <h2 class="city">City, Country</h2>
-    <div class="curr-temp">
-        <div class="svg">
-            <img src="images/clear_sky.svg" />
-        </div>
-        <span class="temp">Temperature</span>
-    </div>
-    <span class="feels-like">Feels like 35&deg;C. Clear sky.</span>
-*/
-const createCityAndTemp = () => {
-    const dateTime = document.createElement("span");
-    dateTime.classList.add("date-time");
+//     <span class="date-time">Date, Time</span>
+//     <h2 class="city">City, Country</h2>
+//     <div class="curr-temp">
+//         <div class="svg">
+//             <img src="images/clear_sky.svg" />
+//         </div>
+//         <span class="temp">Temperature</span>
+//     </div>
+//     <span class="feels-like">Feels like 35&deg;C. Clear sky.</span>
+// */
+// const createCityAndTemp = () => {
+//     const dateTime = document.createElement("span");
+//     dateTime.classList.add("date-time");
 
-    const city = document.createElement("h2");
-    city.classList.add("city");
+//     const city = document.createElement("h2");
+//     city.classList.add("city");
 
-    const currTemp = document.createElement("div");
-    currTemp.classList.add("curr-temp");
+//     const currTemp = document.createElement("div");
+//     currTemp.classList.add("curr-temp");
 
-    const svg = document.createElement("div");
-    svg.classList.add("svg");
+//     const svg = document.createElement("div");
+//     svg.classList.add("svg");
 
-    const img = document.createElement("img");
+//     const img = document.createElement("img");
 
-    const temp = document.createElement("span");
-    temp.classList.add("temp");
+//     const temp = document.createElement("span");
+//     temp.classList.add("temp");
 
-    svg.appendChild(img);
-    currTemp.appendChild(svg);
-    currTemp.appendChild(temp);
+//     svg.appendChild(img);
+//     currTemp.appendChild(svg);
+//     currTemp.appendChild(temp);
 
-    const feelsLike = document.createElement("span");
-    feelsLike.classList.add("feels-like");
+//     const feelsLike = document.createElement("span");
+//     feelsLike.classList.add("feels-like");
 
-    const cityTemp = document.querySelector(".city-temp");
+//     const cityTemp = document.querySelector(".city-temp");
 
-    cityTemp.appendChild(dateTime);
-    cityTemp.appendChild(city);
-    cityTemp.appendChild(currTemp);
-    cityTemp.appendChild(feelsLike);
-};
+//     cityTemp.appendChild(dateTime);
+//     cityTemp.appendChild(city);
+//     cityTemp.appendChild(currTemp);
+//     cityTemp.appendChild(feelsLike);
+// };
 
-/*
-    This function will delete the child elements which is stored
-    inside the weather info card
-*/
-const deleteWeatherInfo = () => {
-    const cityTemp = document.querySelector(".city-temp");
-    while (cityTemp.firstChild) {
-        cityTemp.removeChild(cityTemp.firstChild);
-    }
-};
+// /*
+//     This function will delete the child elements which is stored
+//     inside the weather info card
+// */
+// const deleteWeatherInfo = () => {
+//     const cityTemp = document.querySelector(".city-temp");
+//     while (cityTemp.firstChild) {
+//         cityTemp.removeChild(cityTemp.firstChild);
+//     }
+// };
 
-/*
-    This function will run either when the city does not exist or
-    when there is some problem going on in the backend server
-*/
-const errorTemplate = (image, text) => {
-    const cityTemp = document.querySelector(".city-temp");
-    cityTemp.classList.add("city-not-exist");
+// /*
+//     This function will run either when the city does not exist or
+//     when there is some problem going on in the backend server
+// */
+// const errorTemplate = (image, text) => {
+//     const cityTemp = document.querySelector(".city-temp");
+//     cityTemp.classList.add("city-not-exist");
 
-    const errorLogoContainer = document.createElement("div");
-    errorLogoContainer.classList.add("error-logo-container");
-    const errorLogo = document.createElement("img");
-    errorLogo.classList.add("error-logo");
-    errorLogo.src = image;
-    errorLogoContainer.appendChild(errorLogo);
-    cityTemp.appendChild(errorLogoContainer);
+//     const errorLogoContainer = document.createElement("div");
+//     errorLogoContainer.classList.add("error-logo-container");
+//     const errorLogo = document.createElement("img");
+//     errorLogo.classList.add("error-logo");
+//     errorLogo.src = image;
+//     errorLogoContainer.appendChild(errorLogo);
+//     cityTemp.appendChild(errorLogoContainer);
 
-    const h3 = document.createElement("h3");
-    h3.innerText = text;
-    cityTemp.appendChild(h3);
-};
+//     const h3 = document.createElement("h3");
+//     h3.innerText = text;
+//     cityTemp.appendChild(h3);
+// };
 
-const deleteCityNotFoundTemplate = () => {
-    const cityTemp = document.querySelector(".city-temp");
-    cityTemp.classList.remove("city-not-exist");
-    deleteWeatherInfo();
-};
+// const deleteCityNotFoundTemplate = () => {
+//     const cityTemp = document.querySelector(".city-temp");
+//     cityTemp.classList.remove("city-not-exist");
+//     deleteWeatherInfo();
+// };
 
-// This function updates the weather inforation
-const updateCityAndTemp = (weatherInfo) => {
-    const city = document.querySelector(".city");
-    const img = document.querySelector("img");
-    img.classList.add("weather-logo");
-    const temp = document.querySelector(".temp");
-    const feelsLike = document.querySelector(".feels-like");
-    city.innerText = `${weatherInfo.name}, ${weatherInfo.sys.country}`;
-    temp.innerText = `${Math.round(weatherInfo.main.temp)}°C`;
-    feelsLike.innerText = `Feels like ${Math.round(
-        weatherInfo.main.feels_like
-    )}°C. ${weatherInfo.weather[0].description}.`;
+// // This function updates the weather inforation
+// const updateCityAndTemp = (weatherInfo) => {
+//     const city = document.querySelector(".city");
+//     const img = document.querySelector("img");
+//     img.classList.add("weather-logo");
+//     const temp = document.querySelector(".temp");
+//     const feelsLike = document.querySelector(".feels-like");
+//     city.innerText = `${weatherInfo.name}, ${weatherInfo.sys.country}`;
+//     temp.innerText = `${Math.round(weatherInfo.main.temp)}°C`;
+//     feelsLike.innerText = `Feels like ${Math.round(
+//         weatherInfo.main.feels_like
+//     )}°C. ${weatherInfo.weather[0].description}.`;
 
-    if (weatherInfo.weather[0].main === "Thunderstorm") {
-        img.src = "images/thunderstorm.png";
-    } else if (weatherInfo.weather[0].main === "Drizzle") {
-        img.src = "images/drizzle.png";
-    } else if (weatherInfo.weather[0].main === "Rain") {
-        img.src = "images/rain.png";
-    } else if (weatherInfo.weather[0].main === "Snow") {
-        img.src = "images/snow.png";
-    } else if (weatherInfo.weather[0].main === "Clear") {
-        img.src = "images/clear_sky.png";
-    } else if (weatherInfo.weather[0].main === "Clouds") {
-        img.src = "images/clouds.png";
-    } else {
-        img.src = "images/mist.png";
-    }
-};
+//     if (weatherInfo.weather[0].main === "Thunderstorm") {
+//         img.src = "images/thunderstorm.png";
+//     } else if (weatherInfo.weather[0].main === "Drizzle") {
+//         img.src = "images/drizzle.png";
+//     } else if (weatherInfo.weather[0].main === "Rain") {
+//         img.src = "images/rain.png";
+//     } else if (weatherInfo.weather[0].main === "Snow") {
+//         img.src = "images/snow.png";
+//     } else if (weatherInfo.weather[0].main === "Clear") {
+//         img.src = "images/clear_sky.png";
+//     } else if (weatherInfo.weather[0].main === "Clouds") {
+//         img.src = "images/clouds.png";
+//     } else {
+//         img.src = "images/mist.png";
+//     }
+// };
 
-// This function creates the overlay and loader functionality
-const createOverlayAndLoader = () => {
-    const card = document.getElementById("card");
-    const overlay = document.createElement("div");
-    overlay.classList.add("overlay");
-    const loader = document.createElement("div");
-    loader.classList.add("loader");
-    card.appendChild(overlay);
-    card.appendChild(loader);
-};
+// // This function creates the overlay and loader functionality
+// const createOverlayAndLoader = () => {
+//     const card = document.getElementById("card");
+//     const overlay = document.createElement("div");
+//     overlay.classList.add("overlay");
+//     const loader = document.createElement("div");
+//     loader.classList.add("loader");
+//     card.appendChild(overlay);
+//     card.appendChild(loader);
+// };
 
-// This functions deletes the overlay and loader functionality
-const deleteOverlayAndLoader = () => {
-    try {
-        const overlay = document.querySelector(".overlay");
-        overlay.remove();
-    } catch (error) {}
+// // This functions deletes the overlay and loader functionality
+// const deleteOverlayAndLoader = () => {
+//     try {
+//         const overlay = document.querySelector(".overlay");
+//         overlay.remove();
+//     } catch (error) {}
 
-    try {
-        const loader = document.querySelector(".loader");
-        loader.remove();
-    } catch (error) {}
-};
+//     try {
+//         const loader = document.querySelector(".loader");
+//         loader.remove();
+//     } catch (error) {}
+// };
 
 // This function sends data to the backend
 const sendDataToBacked = async (city) => {
@@ -260,7 +284,7 @@ const sendDataToBacked = async (city) => {
     };
 
     try {
-        const response = await fetch("https://weather-app-6h2o.onrender.com", {
+        const response = await fetch("http://localhost:3000/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -276,4 +300,4 @@ const sendDataToBacked = async (city) => {
 };
 
 // https://weather-app-6h2o.onrender.com/
-// http://localhost:8080/
+// http://localhost:3000/
